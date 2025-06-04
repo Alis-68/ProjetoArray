433a4dcc346bfcbe563ead8b52c3440ef18933d3
yan nunes <yanunesxz@gmail.com>
2025-06-04 11:28:41 -0300

    Arquivo de Aula 04/06

---
+++ array7.js
@@ -0,0 +1,50 @@
+// Método Push
+// let produtos = ['Placa-mãe', 'Processador', 'Memória RAM', 'SSD',];
+// produtos.push('Placa de Vídeo', 'Fonte');
+// console.log(produtos);
+
+// método pop
+// let produtos = ['Placa-mãe', 'Processador', 'Memória RAM', 'SSD'];
+// let ultimoItem = produtos.pop(); // Remove o último item do array
+// console.log("Após pop():", produtos);
+// console.log("item removido:", ultimoItem);
+
+// Método shift
+// let produtos = ['Placa mãe', 'Processador', 'Memória RAM', 'SSD'];
+// let primeiroItem = produtos.shift(); // Remove o primeiro item do array
+// console.log("Após shift():", produtos);
+// console.log("Item removido:", primeiroItem);
+
+
+// Método Unshift
+// let produtos = ['Placa-mãe', 'Processador', 'Memória RAM', 'SSD'];
+// produtos.unshift('Gabinete', 'Cooler');
+// console.log(produtos) Adiciona itens no início do array
+// Método indexOf
+// let produtos = ['Placa-Mae', 'Processador', 'Memória RAM', 'SSD'];
+// let indiceRam = produtos.indexOf('Memória RAM');
+// let indiceHd = produtos.indexOf('HD'); 
+// console.log("índice da Ram:",indiceRam);
+// console.log("índice do HD:",indiceHd); 
+// método includes
+// let produtos = ['Placa-mãe', 'Processador', 'Memória RAM', 'SSD'];
+// let temSsd = produtos.includes('SSD'); // Verifica se o item está no array
+// let temTeclado = produtos.includes('Teclado'); // Verifica se o item está no array
+// console.log("Tem SSD?", temSsd);
+// console.log("Tem Teclado?", temTeclado);
+// Método slice
+// let produtos = ['Placa-mãe', 'Processador', 'Memória RAM', 'SSD', 'Placa de Vídeo'];
+// let core = produtos.slice(1, 4);
+// let video = produtos.slice(4);
+// console.log("Core(slice):", core);
+// console.log("Vídeo(slice):", video);
+// console.log("Array Original(nãomudou):", produtos);
+// Método splice
+// let produtos = ['Placa-mãe', 'Processador', 'Memória RAM', 'SSD', 'Placa de Vídeo'];
+// produtos.slice(2, 1);
+// console.log("Após remover RAM:", produtos);
+// produtos.splice(2, 0, 'HD');
+// console.log("Após adicionar HD:", produtos)
+// produtos.splice(1, 1, 'Cpu Rápida');
+// console.log("Após susbtituir:", produtos);
+
--- arrray6.js
+++ arrray6.js
@@ -2,9 +2,6 @@ let pessoa = {
     nome: "Tom Cruise",
     idade: 62,
     profissao: "Ator",
-    filmes: {"Top Gun", "Mission: Impossible", "Jerry Maguire"}
-    Conjuges: { Mimi Rogers, Nicole Kidman, Katie Holmes },
-
-
-    }
-}
+    filmes: {"Top Gun", "Mission: Impossible", "Jerry Maguire"},
+    Conjuges: {"Mimi Rogers", "Nicole Kidman", "Katie Holmes"},
+} 
